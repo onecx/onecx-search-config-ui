32412e9ccbfa7cdd317ce7f7378feb7d321ac70f
markuczy <martin.kuczynski@capgemini.com>
2024-09-12 13:26:39 +0200

    feat: webpack config adjustments

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const {
   withModuleFederationPlugin,
 } = require('@angular-architects/module-federation/webpack');
 const config = withModuleFederationPlugin({
-  name: 'onecx-search-config-app',
+  name: 'onecx-search-config-ui',
   filename: 'remoteEntry.js',
   exposes: {
     './OneCXSearchConfigComponent':
@@ -43,19 +43,15 @@ const config = withModuleFederationPlugin({
       includeSecondaries: true,
     },
     primeng: { requiredVersion: 'auto', includeSecondaries: true },
-    '@onecx/portal-integration-angular': {
+    '@ngx-translate/core': {
       requiredVersion: 'auto',
-      includeSecondaries: true,
     },
-    '@onecx/keycloak-auth': {
-      requiredVersion: 'auto',
-      includeSecondaries: true,
-    },
+    '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/angular-accelerator': {
       requiredVersion: 'auto',
       includeSecondaries: true,
     },
-    '@onecx/angular-webcomponents': {
+    '@onecx/angular-auth': {
       requiredVersion: 'auto',
       includeSecondaries: true,
     },
@@ -63,9 +59,26 @@ const config = withModuleFederationPlugin({
       requiredVersion: 'auto',
       includeSecondaries: true,
     },
-    '@ngx-translate/core': {
-      strictVersion: false,
-      requiredVersion: '^14.0.0',
+    '@onecx/angular-webcomponents': {
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
+    '@onecx/integration-interface': {
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
+    '@onecx/keycloak-auth': {
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
+    '@onecx/nx-plugin': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/portal-integration-angular': {
+      requiredVersion: 'auto',
+      includeSecondaries: true,
+    },
+    '@onecx/portal-layout-styles': {
+      requiredVersion: 'auto',
+      includeSecondaries: true,
     },
   }),
 
