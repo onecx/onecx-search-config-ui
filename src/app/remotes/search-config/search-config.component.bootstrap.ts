9ef1ac3116f61832bf9f02eaff0ac3f09a0d45b0
markuczy <martin.kuczynski@capgemini.com>
2024-10-01 14:07:00 +0200

    feat: initialize search config after user service is initialize

--- src/app/remotes/search-config/search-config.component.bootstrap.ts
+++ src/app/remotes/search-config/search-config.component.bootstrap.ts
@@ -1,15 +1,36 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
-import { BrowserModule } from '@angular/platform-browser'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { importProvidersFrom } from '@angular/core'
-import { AngularAuthModule } from '@onecx/angular-auth'
-import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents'
-import { environment } from 'src/environments/environment'
-import { OneCXSearchConfigComponent } from './search-config.component'
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
+import { AngularAuthModule } from '@onecx/angular-auth';
+import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents';
+import { environment } from 'src/environments/environment';
+import { OneCXSearchConfigComponent } from './search-config.component';
+import { UserService } from '@onecx/portal-integration-angular';
 
-bootstrapRemoteComponent(OneCXSearchConfigComponent, 'ocx-search-config-component', environment.production, [
-  provideHttpClient(withInterceptorsFromDi()),
-  importProvidersFrom(AngularAuthModule),
-  importProvidersFrom(BrowserModule),
-  importProvidersFrom(BrowserAnimationsModule)
-])
+function userProfileInitializer(userService: UserService) {
+  return async () => {
+    await userService.isInitialized;
+  };
+}
+
+bootstrapRemoteComponent(
+  OneCXSearchConfigComponent,
+  'ocx-search-config-component',
+  environment.production,
+  [
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(AngularAuthModule),
+    importProvidersFrom(BrowserModule),
+    importProvidersFrom(BrowserAnimationsModule),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: userProfileInitializer,
+      deps: [UserService],
+      multi: true,
+    },
+  ],
+);
