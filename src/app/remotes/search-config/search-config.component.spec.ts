7f008de5f09b33595bfe49e6a2a97d0c7923ddbe
SylwiaKruszynska1 <221315718+SylwiaKruszynska1@users.noreply.github.com>
2026-09-01 11:22:17 +0200

    fix: removed duplicate setup

--- src/app/remotes/search-config/search-config.component.spec.ts
+++ src/app/remotes/search-config/search-config.component.spec.ts
@@ -60,20 +60,6 @@ const createSpyObj = (
 };
 
 describe('OneCXSearchConfigComponent', () => {
-  Object.defineProperty(window, 'matchMedia', {
-    writable: true,
-    value: jest.fn().mockImplementation((query) => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addListener: jest.fn(), // deprecated
-      removeListener: jest.fn(), // deprecated
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
-  });
-
   const searchConfigServiceSpy = {
     ...createSpyObj('searchConfigService', [
       'getSearchConfigInfos',
